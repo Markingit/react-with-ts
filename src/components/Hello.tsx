36f14647c1544fb38cccfac61ec98a1f91c25de0
matengfei <matengfei@imzhiliao.com>
2020-05-08 23:41:20 +0800

    add .

---
+++ src/components/Hello.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface IHelloProps {
+    message: string
+}
+
+// const Hello = (props: IHelloProps) => {
+//     return <h2>{props.message}</h2>
+// }
+
+
+const Hello: React.FC<IHelloProps> = (props: IHelloProps) => {
+        return <h2>{props.message}</h2>
+    }
+export default Hello
